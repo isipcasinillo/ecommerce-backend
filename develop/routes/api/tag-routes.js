dfd9e7fcc29cd9cb71147cda5646d3b32b216618
Cyril Isip <cisip@Cyrils-MacBook-Pro.local>
2021-10-11 22:52:28 -0500

    refactor code

---
+++ develop/routes/api/tag-routes.js
@@ -0,0 +1,101 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include:
+    {
+      model:Product,
+      through: ProductTag
+    }
+  })
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findOne({
+    where:
+    {
+      id: req.params.id
+    },
+    include:
+    {
+      model:Product,
+      through: ProductTag
+    }
+  })
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  Tag.create(req.body)
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  Tag.update(req.body,{
+    where:
+    {
+      id:req.params.id
+    }
+  })
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where:
+    {
+      id:req.params.id
+    }
+  })
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+module.exports = router;
